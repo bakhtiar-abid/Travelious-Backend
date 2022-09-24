fc7d632988b51fa7807dd1e065713ff5427f8ccd
bakhtiarabid <abid.bu16@gmail.com>
2022-09-24 09:39:54 +0600

    Server Updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
